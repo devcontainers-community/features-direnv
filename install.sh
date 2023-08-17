1368185b4a56ca2e3afeb6eafb9bb6b554320436
Jacob Hummer <jcbhmr@outlook.com>
2023-08-17 14:28:02 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 set -ex
 
-cat <<EOF
-----------------------------------------------
+if [[ $VERSION == system ]]; then
+  sudo apt-get update
+  sudo apt-get install -y direnv
+else
+  curl -sS https://webi.sh/gh | sh
 
-               YOU ARE AWESOME!
+  # https://stackoverflow.com/a/48679640/19522682
+  architecture=""
+  case $(uname -m) in
+      i386)   architecture="386" ;;
+      i686)   architecture="386" ;;
+      x86_64) architecture="amd64" ;;
+      arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
+  esac
+  
+  if [[ $VERSION == latest ]]; then
+    gh release download -p "direnv.linux-$architecture"
+  else
+    gh release download "v$VERSION" -p "direnv.linux-$architecture"
+  fi
 
-----------------------------------------------
-EOF
+  chmod +x direnv.linux-*
+  mv direnv.linux-* /usr/local/bin/direnv
+fi
